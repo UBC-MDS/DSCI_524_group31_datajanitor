a9baff313e77e342680121dc3f14613f55feeb85
karanbayns <karanbains1234@gmail.com>
2026-01-08 15:42:46 -0800

    Create detect_outliers.py

---
+++ src/datajanitor/detect_outliers.py
@@ -0,0 +1,14 @@
+def detect_outliers(df):
+    """
+    Detect and remove outliers in the df DataFrame
+    """
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A pandas DataFrame
+
+    Returns
+    -------
+    ...
+    """
