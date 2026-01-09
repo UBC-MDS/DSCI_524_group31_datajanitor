3c95e54a12db60c30a8fbb1fb653fea8d33f8391
karanbayns <karanbains1234@gmail.com>
2026-01-08 16:17:02 -0800

    Add the documentation for detect_outliers.py

--- src/datajanitor/detect_outliers.py
+++ src/datajanitor/detect_outliers.py
@@ -1,13 +1,15 @@
-def detect_outliers(df):
+def detect_outliers(df, multiplier: 1.5):
     """
-    Detect and remove outliers in the df DataFrame
-    """
+    Detect and remove outliers in the df DataFrame using the interquartile range
 
     Parameters
     ----------
     df : pd.DataFrame
         A pandas DataFrame
+    multiplier: float
+        The multiplier for the interquartile range. Default is 1.5
 
     Returns
     -------
-    ...
+    A pandas DataFrame which is df with outliers removed according to the multipler
+    """
