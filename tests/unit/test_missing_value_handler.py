96c2e0c358676edd77bca76c31b71a91c1e99bf2
Yasaman Eftekharypour <yasi.mailak@gmail.com>
2026-01-16 23:12:52 -0800

    input validation added in the missing_value_handler.py and it's edgecase also added to test_missing_value_handler.py

--- tests/unit/test_missing_value_handler.py
+++ tests/unit/test_missing_value_handler.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import numpy as np
+import pytest
+from datajanitor import missing_value_handler
 
+
+def test_missing_value_handler_edge_cases(data):
+    """
+    Verify missing_value_handler raises correct errors
+    and handles missing values properly.
+    """
+    
+    # TypeError is expected
+    with pytest.raises(TypeError):
+        missing_value_handler([1, 2, 3])
+
+    # ValueError is expected
+    df = pd.DataFrame({"A": [1, np.nan]})
+
+    with pytest.raises(ValueError):
+        missing_value_handler(df, method="invalid")
+        
+    
